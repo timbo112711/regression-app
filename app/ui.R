7117dc55ece1a87a38dcaf8a59a478b9c2f40e06
Tim McWilliams <tmcwilliams1127@gmail.com>
2019-09-24 19:59:49 -0400

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -1,3 +1,6 @@
+# The Regression Shop
+# V 1.0
+# This web app is built to perform both simple and multiple linear regression.
 library(shiny)
 
 ## Start the UI renderer
@@ -24,28 +27,33 @@ shinyUI(
       uiOutput("varselect1"),
       # Variable 2
       uiOutput("varselect2"),
-      # Variable 3
-      uiOutput("varselect3"),
-      # Variable 4
-      uiOutput("varselect4"),
-      # Variable 5
-      uiOutput("varselect5"),
-      # Variable 6
-      uiOutput("varselect6"),
+      # Instructions for using this app
+      h2("Instructions"),
+      p("Step 1: Upload your dataset."),
+      p("Step 2: Choose your y variable. This is the variable you are looking to predict."),
+      p("Step 3: Choose your x variable(s). The variable(s) you want to use to predict your y."),
+      p("Step 4: Use the Data, Summary Statistics, Histograms, and Correlation tabs to perform EDA
+        (Exploratory Data Analysis). This is an important step which will allow you to find issues with your data."),
+      p("Step 5: Use the Modeling and Residuals tabs to build and assess your model. IMPORTANT NOTE: You are allowed to
+        take away, or add in, variables via step 3. This will change your model in real time."),
+      h4("Auto Variable Selection Modeling Feature"),
+      p("The last tab uses an automatic variable selection method to auotmatically select the best variables for your model.
+        Please see that tab for more detail and instructions."),
       HTML('</br>'),
       radioButtons(
         'format',
         h5('Document format'),
-        c('PDF', 'HTML', 'Word'),
+        c('HTML', 'Word'),
         inline = TRUE
         ),
-      downloadButton('downloadReport'),
-      # Instructions for using this app
-      h2("Instructions"),
-      p("Coming soon to an App near you!!!!!!!")
+      downloadButton('downloadReport')
       ),
     # The main panel where all the shit happens
     mainPanel(
+      # Make it so errors don't show up on the page
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"),
       tabsetPanel(type = "tabs",
                   tabPanel("Data",
                            HTML("</br> Please upload your dataset! </br> </br>"),
@@ -65,45 +73,15 @@ shinyUI(
                              label="Interpretation",
                              value="Enter text...")
                            ),
-                  
                   tabPanel("Histograms",
                            h4("Distributions For Each Variable"),
                            uiOutput("histogramText"),
-                           plotOutput("distPlot_iv1"),
+                           plotOutput("distPlot_iv"),
                            textInput(
                              "text_hist_iv1",
                              label="Interpretation",
                              value="Enter text..."
                              ),
-                           
-                           plotOutput("distPlot_iv2"),
-                           textInput(
-                             "text_hist_iv2",
-                             label="Interpretation",
-                             value="Enter text..."
-                             ),
-                  
-                           plotOutput("distPlot_iv3"),
-                           textInput(
-                             "text_hist_iv3",
-                              label="Interpretation",
-                             value="Enter text..."
-                             ),
-                          
-                           plotOutput("distPlot_iv4"),
-                           textInput(
-                             "text_hist_iv4",
-                             label="Interpretation",
-                             value="Enter text..."
-                             ),
-      
-                           plotOutput("distPlot_iv5"),
-                           textInput(
-                             "text_hist_iv5",
-                             label="Interpretation",
-                             value="Enter text..."
-                             ),
-                          
                            plotOutput("distPlot_dv"),
                            textInput(
                              "text_hist_dv",
@@ -111,7 +89,6 @@ shinyUI(
                              value="Enter text..."
                              )
                            ),
-    
                   tabPanel("Correlations",
                            h4("Correlation Between Variables"),
                            uiOutput("correlationText"),
@@ -165,6 +142,7 @@ shinyUI(
                            
                    tabPanel("Auto Variable Selection Modeling",
                             h4("Auto Modeling Output"),
+                            h5("ALL VARIABLES MUST BE SELECTED FOR THIS FEATURE."),
                             p("The below table displays the auto variable selection using the backward selection method.
                               There are 5 subsets of data being taking into consideration when running throught the auto variable selection.
                               Each subset may or may not have all of the variables present in the data. The first subset (1) contains the most
